c8117392eb9efa35d33995e198723cd885bf5c92
v3n7 <teodor-13@yandex.ru>
2020-05-12 00:51:41 +0700

    Selected row

--- 6_redux/src/shared/components/Table/index.js
+++ 6_redux/src/shared/components/Table/index.js
@@ -3,6 +3,22 @@ import React from "react";
 import styles from "./Table.module.css";
 
 export default function Table({ className, columns, data }) {
+  const [selectedRow, setSelectedRow] = React.useState();
+
+  const onClick = (event) => {    
+    event.preventDefault();
+    
+    //const index = Number.parseInt(event.currentTarget.children[0].innerText);
+
+    if (!!selectedRow) {
+      selectedRow.removeAttribute("class");
+    }
+
+    event.currentTarget.setAttribute("class", styles.selected);
+
+    setSelectedRow(event.currentTarget);
+  };
+
   return (
     <table className={[styles.table, className].join(" ")}>
       <thead>
@@ -16,7 +32,7 @@ export default function Table({ className, columns, data }) {
       </thead>
       <tbody>
         {data.map((item, index) => (
-          <tr key={index}>
+          <tr key={index} onClick={onClick}>
             {columns.map(({ dataIndex }, index) => (
               <td key={index}>
                 <span>{item[dataIndex] || ""}</span>
